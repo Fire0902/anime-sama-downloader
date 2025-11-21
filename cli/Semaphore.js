c11eaa23e9a57dda309a4b298cf0bb8f078643e7
hayem <marc.haye@etu.umontpellier.fr>
2025-11-21 10:49:30 +0100

    renamae running to workers

--- cli/Semaphore.js
+++ cli/Semaphore.js
@@ -1,7 +1,7 @@
 class Semaphore {
-    constructor(maxRunning) {
-        this.maxRunning = maxRunning;
-        this.running = 0;
+    constructor(maxWorkers) {
+        this.maxWorkers = maxWorkers;
+        this.runningWorkers = 0;
         this.queue = [];
     }
 
@@ -9,23 +9,20 @@ class Semaphore {
     * Add a worker in process queue.
     */
     async acquire() {
-        if (this.running < this.maxRunning) {
-            this.running++;
+        if (this.runningWorkers < this.maxWorkers) {
+            this.runningWorkers++;
             return;
         }
-
-        return new Promise(resolve => {
-            this.queue.push(resolve);
-        });
+        return new Promise(resolve => this.queue.push(resolve));
     }
 
     /**
     * Release a worker in process queue.
     */
     release() {
-        this.running--;
+        this.runningWorkers--;
         if (this.queue.length > 0) {
-            this.running++;
+            this.runningWorkers++;
             const resolve = this.queue.shift();
             resolve();
         }
