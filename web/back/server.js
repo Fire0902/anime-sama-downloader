be80e4d7e3df9996ba3650c7c20a1d2c8170e2e4
Damien Rostaing <damien.rostaing@gmail.com>
2025-12-09 12:44:59 +0100

    now its extrack season and have some css (not beautyfull at all)

--- web/back/server.js
+++ web/back/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const Browser = require('../../engine/Browser');
+const { removeScans } = require('../../engine/DownloadService');
 const { websiteUrl } = require("../../config/config");
-const { extractAnimeTitles } = require("../../engine/Scrapper");
+const { extractAnimeTitles, extractSeasonsWithScans } = require("../../engine/Scrapper");
 
 
 const app = express();
@@ -16,8 +17,6 @@ app.post("/input", async (req, res) => {
 
     const page = await Browser.newPage();
 
-
-
     console.log("Reçu du frontend:");
     console.log("Valeur complète:", value);
     console.log("Dernière lettre:", lastChar);
@@ -30,6 +29,18 @@ app.post("/input", async (req, res) => {
     const animesTitle = await extractAnimeTitles(page);
 
     res.json({ animesTitle: animesTitle });
+});
+app.post("/seasons", async (req, res) => {
+    const { animeUrl } = req.body;
+
+    const page = await Browser.newPage();
+
+    await page.goto(animeUrl, {
+        waitUntil: 'networkidle2'
+    });
+    const animeSeasonsWithScan = await extractSeasonsWithScans(page);
+    const animeSeasons = removeScans(animeSeasonsWithScan);
+    res.json({ animeSeasons: animeSeasons });
 });
 
 app.listen(PORT, () => {
--- web/front/js/script.js
+++ web/front/js/script.js
@@ -1,37 +1,67 @@
 const input = document.getElementById("live-input");
 
 let debounceTimer;
+let selectedAnime = {}; // look like that {"One Piece": "https://anime-sama.eu/catalogue/one-piece/"}
+let selectedSeason = {}; // look like that {"Saga 1 (East Blue)": "https://anime-sama.eu/catalogue/one-piece/saison1/vostfr"}
 
 input.addEventListener("input", (e) => {
-  const value = e.target.value;
+    const value = e.target.value;
 
-  clearTimeout(debounceTimer);
+    clearTimeout(debounceTimer);
 
-  debounceTimer = setTimeout(async () => {
-    try {
-      const response = await fetch("http://localhost:3000/input", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          value,
-          lastChar: value.slice(-1) || null,
-        })
-      });
+    debounceTimer = setTimeout(async () => {
+        try {
+            const response = await fetch("http://localhost:3000/input", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    value,
+                    lastChar: value.slice(-1) || null,
+                })
+            });
 
-      const animesTitle = (await response.json()).animesTitle;
-      const animesContainer = document.getElementById("anime-container");
+            const animesTitle = (await response.json()).animesTitle;
+            const animesContainer = document.getElementById("animes-container");
+            const seasonsContainer = document.getElementById("seasons-container");
 
-      animesContainer.innerHTML = "";
+            animesContainer.innerHTML = "";
 
-      for (const [animeName, url] of Object.entries(animesTitle)) {
-        const anime = document.createElement("div");
-        anime.textContent = animeName;
-        animesContainer.appendChild(anime);
-      }
-    } catch (err) {
-      console.error("error: ", err);
-    }
-  }, 300);
+            for (const [animeName, url] of Object.entries(animesTitle)) {
+                const anime = document.createElement("div");
+                anime.addEventListener("click", async () => {
+                    selectedAnime[animeName] = url;
+                    animesContainer.innerHTML = "";
+                    input.value = animeName;
+                    const response = await fetch("http://localhost:3000/seasons", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            animeUrl: url
+                        })
+                    });
+                    const animeSeasons = (await response.json()).animeSeasons;
+                    for(const season of animeSeasons){
+                        season.link = url + season.link;
+                        const seasonDiv = document.createElement("div");
+                        seasonDiv.textContent = season.name
+                        seasonDiv.classList.add("season");
+                        seasonDiv.addEventListener("click", () => {
+                            selectedSeason[season.name] = season.link;
+                            
+                        })
+                        seasonsContainer.appendChild(seasonDiv);
+                    }
+                });
+                anime.classList.add("anime");
+                anime.textContent = animeName;
+                animesContainer.appendChild(anime);
+            }
+        } catch (err) {
+            console.error("error: ", err);
+        }
+    }, 300);
 });
