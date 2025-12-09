85bd0948d58a43b23ad53f90c12bea1398e77b33
Damien Rostaing <damien.rostaing@gmail.com>
2025-12-09 11:52:16 +0100

    add timer, not to spam fetch

--- web/front/js/script.js
+++ web/front/js/script.js
@@ -1,31 +1,38 @@
 const input = document.getElementById("live-input");
 
-input.addEventListener("input", async (e) => {
+let debounceTimer;
+
+input.addEventListener("input", (e) => {
   const value = e.target.value;
 
-  try {
-    const response = await fetch("http://localhost:3000/input", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        value,
-        lastChar: value.slice(-1) || null,
-        timestamp: Date.now()
-      })
-    });
-    const animesTitle = (await response.json()).animesTitle;
-    const animesContainer = document.getElementById("anime-container");
-    console.log(animesTitle);
-    animesContainer.innerHTML = "";
+  clearTimeout(debounceTimer);
 
-    for(const [animeName, url] of Object.entries(animesTitle)){
+  debounceTimer = setTimeout(async () => {
+    try {
+      const response = await fetch("http://localhost:3000/input", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          value,
+          lastChar: value.slice(-1) || null,
+          timestamp: Date.now()
+        })
+      });
+
+      const animesTitle = (await response.json()).animesTitle;
+      const animesContainer = document.getElementById("anime-container");
+
+      animesContainer.innerHTML = "";
+
+      for (const [animeName, url] of Object.entries(animesTitle)) {
         const anime = document.createElement("div");
         anime.textContent = animeName;
         animesContainer.appendChild(anime);
+      }
+    } catch (err) {
+      console.error("error: ", err);
     }
-  } catch (err) {
-    console.error("error: ", err);
-  }
+  }, 300); // ⏱️ attends 300ms après la dernière frappe
 });
