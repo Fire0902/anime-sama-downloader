7c437dd6e27ccb0c5365162cbae2ef3ea6da24ac
Damien Rostaing <damien.rostaing@gmail.com>
2025-12-14 19:34:46 +0100

    add some test

--- test/ScrapperTest.test.js
+++ test/ScrapperTest.test.js
@@ -1,23 +1,21 @@
 import assert from "node:assert";
-import { Browser } from "../engine/Browser.js";
-import { extractAnimeTitles } from "../engine/Scrapper.js";
-
-export async function mochaGlobalSetup() {
-  globalThis.browser = await Browser.newPage();
-}
+import Browser from "../engine/utils/web/Browser.js";
+import Scrapper from "../engine/utils/web/Scrapper.js";
 
 describe('Scrapper', function(){
   describe("#extractAnimeTitles()", function () {
     it("should return an object with animes titles and url bind", async function () {
       const testUrl = "https://anime-sama.eu/catalogue/?search=one+p"
-      const animesPage = await globalThis.browser.newPage();
-      await animesPage.goto(testUrl, {
+      const page = await Browser.newPage();
+      await page.goto(testUrl, {
         waitUntil: 'networkidle2'
       });
-      const animes = await extractAnimeTitles(animesPage);
+      const animes = await Scrapper.extractAnimeTitles(page);
+
+      console.log(animes)
 
       assert.strictEqual(
-        animes.includes("One Piece", "One Punch Man"), 
+        Object.keys(animes).includes("One Piece", "One Punch Man"), 
         true
       );
     });
