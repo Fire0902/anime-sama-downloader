03aa00a74f623361318344d79d7fb2debf15ac82
Damien Rostaing <damien.rostaing@gmail.com>
2025-12-15 08:48:47 +0100

    add one test

--- test/ScrapperTest.test.js
+++ test/ScrapperTest.test.js
@@ -2,7 +2,7 @@ import assert from "node:assert";
 import Browser from "../engine/utils/web/Browser.js";
 import Scrapper from "../engine/utils/web/Scrapper.js";
 
-describe('Scrapper', function(){
+describe('Scrapper', function () {
   describe("#extractAnimeTitles()", function () {
     it("should return an object with animes titles and url bind", async function () {
       const testUrl = "https://anime-sama.eu/catalogue/?search=one+p"
@@ -12,12 +12,43 @@ describe('Scrapper', function(){
       });
       const animes = await Scrapper.extractAnimeTitles(page);
 
-      console.log(animes)
+      Browser.close();
+
+      let result = false;
+
+      if(Object.keys(animes).includes("One Piece") 
+        || Object.keys(animes).includes("One Punch Man")){
+          result = true;
+      }
 
       assert.strictEqual(
-        Object.keys(animes).includes("One Piece", "One Punch Man"), 
+        result,
         true
       );
     });
   });
+  describe("#extractSeasonsWithScans()", function () {
+    it("should return seasons with their url", async function () {
+      const testUrl = "https://anime-sama.eu/catalogue/one-piece/"
+      const page = await Browser.newPage();
+      await page.goto(testUrl, {
+        waitUntil: 'networkidle2'
+      });
+      const seasons = await Scrapper.extractSeasonsWithScans(page);
+
+      Browser.close();
+
+      let result = false;
+
+      if(Object.keys(seasons).includes("Saga 1 (East Blue)") 
+        || Object.keys(seasons).includes("One Piece Log: Fish-Man Island Saga")){
+          result = true;
+      }
+
+      assert.strictEqual(
+        result,
+        true
+      );
+    })
+  })
 });
