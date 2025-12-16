213ce319c522f55ff186a8ed2f59a4340c92dd40
hayem <marc.haye@etu.umontpellier.fr>
2025-12-16 15:03:52 +0100

    add docs

--- engine/utils/Log.ts
+++ engine/utils/Log.ts
@@ -2,18 +2,45 @@ import { Logger } from "tslog";
 import Config from "../config/Config.ts";
 import fs from "node:fs/promises";
 
+/**
+ * @see https://www.npmjs.com/package/tslog
+ */
 export default class Log {
-	static create(name: string): Logger<unknown> {
-		const logger = new Logger({ name: name });
 
-		this.attachTransport(logger);
+	/**
+	 * Creates and configure a new logger instance.
+	 * @param name by default 'Logger'
+	 * @returns new logger
+	 */
+	static create(name: string = 'Logger'): Logger<any> {
+		const logger = new Logger({
+			name: name,
+			type: Config.logDefaultType,
+			minLevel: Config.logMinLevel
+		});
+
+		this.#attachTransport(logger);
 		return logger;
 	}
 
-	static attachTransport(logger: Logger<unknown>){
-		logger.attachTransport(async (logObj) => {
-			await fs.writeFile(Config.logsPath, '', Config.downloadEncoding);
-			await fs.appendFile(Config.logsPath, JSON.stringify(logObj) + "\n")
-		});
+	/**
+	 * Attaches logger to transport into log files. 
+	 * Given logger will write in logs file everytime a log method is called.
+	 * @param logger logger object to attach
+	 */
+	static #attachTransport(logger: Logger<any>) {
+		logger.attachTransport(async (logObj) => await this.#createFile(logObj));
+	}
+
+	/**
+	 * Create recursively log file.
+	 * @param logObj content to append to file
+	 */
+	static async #createFile(logObj: any){
+		await fs.mkdir(Config.logPath, { recursive: true });
+		const filePath = `${Config.logPath}/${new Date().toDateString()}.txt`
+
+		await fs.writeFile(filePath, '', Config.defaultEncoding);
+		await fs.appendFile(filePath, JSON.stringify(logObj) + "\n")
 	}
 }
