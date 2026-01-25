77faca6f396b9665f806367a9043a1bd7a8ea747
hayem <marc.haye@etu.umontpellier.fr>
2026-01-25 15:36:01 +0100

    usage of FileUtils in logs

--- src/utils/log/Log.ts
+++ src/utils/log/Log.ts
@@ -1,18 +1,17 @@
 import { Logger } from "tslog";
 import Config from "../../config/Config.ts";
-import fsp from "node:fs/promises";
-import fs from "node:fs"
+import FileUtils from "../file/FileUtils.ts";
+
 /**
  * Class for creating and configuring loggers
  * @see https://www.npmjs.com/package/tslog
  */
 export default class Log {
-
 	/**
 	 * Creates and configure a new logger instance.
 	 * @param name logger name, by default 'Logger'.
-	 * @param type log type, by default 'hidden'. 
-	 * @param minLevel 
+	 * @param type log type, by default 'hidden'.
+	 * @param minLevel
 	 * @returns new logger
 	 * @see https://www.npmjs.com/package/tslog
 	 */
@@ -32,27 +31,19 @@ export default class Log {
 	}
 
 	/**
-	 * Attaches logger to transport into log files. 
+	 * Attaches logger to transport into log files.
 	 * Given logger will write in logs file everytime a log method is called.
 	 * @param logger logger object to attach
 	 * @see https://www.npmjs.com/package/tslog
 	 */
 	private static attachTransport(logger: Logger<any>) {
-		logger.attachTransport(async (logObj) => await this.createFile(logObj));
-	}
-
-	/**
-	 * Create recursively log file.
-	 * @param logObj content to append to file
-	 * @see https://www.npmjs.com/package/tslog
-	 */
-	private static async createFile(logObj: any){
-		await fsp.mkdir(Config.logPath, { recursive: true });
-		const filePath = `${Config.logPath}/${new Date().toDateString()}.txt`
-
-		if (!fs.existsSync(filePath)){
-			await fsp.writeFile(filePath, '', Config.defaultEncoding);
-		}
-		await fsp.appendFile(filePath, JSON.stringify(logObj, null, 1) + "\n")
+		logger.attachTransport(
+			async (logObj) =>
+				await FileUtils.append(
+					Config.logPath,
+					`${new Date().toDateString()}.${Config.logFileType}`,
+					logObj
+				),
+		);
 	}
 }
