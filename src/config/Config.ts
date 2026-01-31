c6a5567d93cc6fc785b56c843e3dc6196078efd5
hayem <marc.haye@etu.umontpellier.fr>
2026-01-31 13:38:35 +0100

    rename downloadJSONPath to autoDownloaderDataPath

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -21,8 +21,8 @@ export default class Config{
     /** Maximum number of videos downloaded simultaneously */
     static readonly maxSimultVideos: number = 2;
 
-    /** */
-    static readonly downloadJSONPath: string = 'json/animes.json';
+    /** Folder where all data will be taken from auto-downloader */
+    static readonly autoDownloaderDataPath: string = 'json/animes.json';
 
     /** Folder where all videos will be downloaded */
     static readonly downloadPath: string = 'animes';
@@ -54,6 +54,7 @@ export default class Config{
     /** Browser user agent */
     static readonly userAgent: string = "Mozilla/5.0";
 
+    /** Default network event to wait for */
     static readonly defaultWaitUntil: PuppeteerLifeCycleEvent = 'networkidle2';
 
     /** Time to wait for web page before timeout */
@@ -64,31 +65,27 @@ export default class Config{
 
     // ----- PUPPETEER - ANIMES -----
 
-    /**  */
+    /** Default network event to wait for */
     static readonly animeSearchWaitUntil: PuppeteerLifeCycleEvent = 'networkidle2';
 
-    /** */
+    /** 
+     * Anime catalog list HTML identifier.
+    */
     static readonly animeSearchPageId: string = "list_catalog";
 
-    /** */
+    /** 
+     * Anime catalog list HTML selector.
+    */
     static readonly animeSearchPageSelector: string = `#${this.animeSearchPageId}`;
 
     // ----- PUPPETEER - SEASONS -----
 
-    /** */
+    /** Default network event to wait for */
     static readonly seasonSearchWaitUntil: PuppeteerLifeCycleEvent = 'networkidle2';
 
-    /** */
+    /** Anime catalog list HTML selector */
     static readonly seasonsPageSelector: string = 
     "div.flex.flex-wrap.overflow-y-hidden.justify-start.bg-slate-900.bg-opacity-70.rounded.mt-2.h-auto a";
-
-    // ----- DEBUG -----
-
-    /** Will take a screenshot each loaded page. Mostly used for debugging */
-    static readonly enableScreenshots: boolean = true;
-
-    /** Where screenshots will be saved */
-    static readonly screenshotsPath: string = "screenshots";
 
     // ----- LOGS -----
 
@@ -103,5 +100,13 @@ export default class Config{
 
     /** Logger minimum level to be visible or hidden */
     static readonly logMinLevel: number = 2;
+
+    // ----- DEBUG -----
+
+    /** Will take a screenshot each loaded page. Mostly used for debugging */
+    static readonly enableScreenshots: boolean = true;
+
+    /** Where screenshots will be saved */
+    static readonly screenshotsPath: string = "screenshots";
 
 }
